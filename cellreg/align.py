8500f4b8b507203d21f2786232e1f77838c6c37f
digicosmos86 <yang_xu@brown.edu>
2023-10-10 13:41:05 -0400

    add package files

---
+++ cellreg/align.py
@@ -0,0 +1,5 @@
+"""Performs FoV alignment.
+
+From a given directory of average images, performs image segmentation and saves output
+to a given directory.
+"""
---
+++ cellreg/denoise.py
@@ -0,0 +1,5 @@
+"""Performs denoising.
+
+Denoises input images in a given directory with given model and output to an
+output directory.
+"""
