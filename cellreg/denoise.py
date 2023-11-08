52b34b0e8be7461c55d869ccb1ce88631be0b770
Ford McDonald <fmcdona4@gpu2105.oscar.ccv.brown.edu>
2023-11-08 13:44:22 -0500

    denoise component with typer cli

--- cellreg/denoise.py
+++ cellreg/denoise.py
@@ -4,23 +4,21 @@ Denoises input images in a given directory with given model and output to an
 output directory.
 """
 
-import argparse
+import typer
 import os
 from deepcad.test_collection import testing_class
 
-def main():
-    parser = argparse.ArgumentParser(description="Performs denoising on input images.")
+app = typer.Typer()
+
+@app.command()
+def main(data_input: str = typer.Option(..., "--data-input", help="Path to the directory containing input images to be denoised"),
+          output_directory: str = typer.Option("./results", "--output-directory", help="Output directory for the denoised images"),
+          denoise_model_path: str = typer.Option(..., "--denoise-model-path", help="Path to the denoise model to use")):
+    """
+    Performs denoising on input images.
+    """
     
-    parser.add_argument("--data_input", type=str, required=True, help="The number of frames to be tested")
-    parser.add_argument("--output_directory", type=str, default="./results",
-                        help="Output directory for the denoised images")
-    parser.add_argument("--denoise_model_path", type=str, required=True, help="Path to the denoise model to use")
-    parser.add_argument("--nframes", type=int, default=5640, help="Path to the denoise model to use")
-
-
-    args = parser.parse_args()
-
-    test_datasize = len([f for f in os.listdir(args.data_input) if os.path.isdir(f)])
+    test_datasize = len([f for f in os.listdir(data_input) if os.path.isdir(f)])
 
     test_dict = {
         'patch_x': 150,
@@ -28,10 +26,11 @@ def main():
         'patch_t': 150,
         'overlap_factor': 0.6,
         'scale_factor': 1,
-        'datasets_path': args.data_input,
-        'test_datasize': args.nframes,
-        'pth_dir': args.denoise_model_path,
-        'output_dir': args.output_directory,
+        'test_datasize': test_datasize,
+        'datasets_path': data_input,
+        'pth_dir': '.',
+        'denoise_model': denoise_model_path,
+        'output_dir': output_directory,
         'fmap': 16,
         'GPU': '0',
         'num_workers': 0,
@@ -43,4 +42,4 @@ def main():
     tc.run()
 
 if __name__ == "__main__":
-    main()
+    app()
